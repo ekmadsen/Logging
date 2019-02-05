3c86759887f020b04dea134f7177c621182fb8d3
Erik Madsen <ekmadsen@comcast.net>
2019-02-05 13:32:00 -0600

    Added SQL scripts.

---
+++ GetPerformaceLogsByCorrelationId.sql
@@ -0,0 +1,4 @@
+﻿select p.*
+from [Logging].PerformanceLogsLastDay p
+where p.CorrelationId = '32EA5319-2A4C-43FA-8F42-C7116A73B87D'
+order by p.Id asc
---
+++ GetPerformanceLogs.sql
@@ -0,0 +1,4 @@
+﻿select p.*
+from [Logging].PerformanceLogsLastDay p
+where p.Timestamp > '02/05/2019 9:07 AM'
+order by p.Id asc
